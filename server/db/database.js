6d09ce2b9cd6e328a64c70b600888dd2e701d1cd
tYp3_cOnfUsIoN <shanayasharma7077@gmail.com>
2026-05-16 10:57:37 +0530

    Update database.js

--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,10 @@
 const Database = require("better-sqlite3");
 const path = require("path");
 
-const db = new Database(path.join(__dirname, "nourishmitra.db"));
+const db = new Database(
+  process.env.DB_PATH || path.join(__dirname, "nourishmitra.db")
+);
+
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS users (
